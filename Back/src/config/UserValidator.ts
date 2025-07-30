325716145bcbe90185c318c37531f4cf0cbd96b8
Samueldnz <samuelsd@dcc.ufrj.br>
2025-07-30 11:06:20 -0300

    add: organization

--- Back/src/config/UserValidator.ts
+++ Back/src/config/UserValidator.ts
@@ -4,19 +4,24 @@ const user = z.object({
     cpf: z.string().trim().regex(/^\d{11}$/, {
         message: 'CPF deve conter exatamente 11 dígitos numéricos'
     }),
+
     telefone: z.string().trim().regex(/^\d{8,15}$/, {
         message: 'Telefone deve conter entre 8 e 15 dígitos numéricos'
     }),
+
     name: z
         .string()
         .trim()
         .min(5, "Nome deve ter no mínimo 5 caracteres")
         .max(40, "Nome deve ter no máximo 40 caracteres"),
+
     email: z.email("Email inválido"),
+
     password: z.string().regex(
         /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{6,}$/,
         { message: 'Senha deve conter no mínimo 6 caracteres, incluindo letras e números' }
     ),
+    
     avatarUrl: z.url("URL inválida").optional()
 });
 
