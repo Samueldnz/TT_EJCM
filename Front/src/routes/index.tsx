fa62c63e9ca9ff7b5fa1da15d35af0e4ceeddfed
Samueldnz <samuelsd@dcc.ufrj.br>
2025-07-30 13:41:17 -0300

    add: login screen

--- Front/src/routes/index.tsx
+++ Front/src/routes/index.tsx
@@ -1,15 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import Register from "../pages/Register";
+import Login from "../pages/Login";
 
 const router = createBrowserRouter([
   {
+    path: "/login",
+    element: <Login />,
+    errorElement: <div className="text-center mt-20 text-red-500">Página não encontrada.</div>,
+  },
+  {
     path: "/register",
     element: <Register />,
-    errorElement: <div className="text-center mt-20 text-red-500">Página não encontrada.</div>, // opcional
+    errorElement: <div className="text-center mt-20 text-red-500">Página não encontrada.</div>,
   },
   {
     path: "/",
-    element: <div className="text-center mt-20">Página inicial (placeholder)</div>, // temp
+    element: <div className="text-center mt-20">Página inicial (teste)</div>,
   }
 ]);
 
